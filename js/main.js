482a710af2b99372ff85e713259868faf31f1386
ByXiaoXie <xiaoxiewg@gmail.com>
2020-12-25 07:12:07 +0800

    nav update
    nav update add js

--- js/main.js
+++ js/main.js
@@ -23,3 +23,35 @@ $(".box_item_left").click(function(){
 $(".box_item_right").click(function(){
     alert("椎名ましろが世界一かわいい( ´ ω ` )");
 })
+
+$(".buttom-item a").click(function(){
+    alert("假的别点了CDD( ´ ω ` )");
+})
+
+$(".v-switcher_header_ul a").click(function(){
+    var index = $("ul a").index(this);
+    $("a").removeClass("v-switcher_header_a_select");
+    $(".v-switcher_header_ul a").eq(index).addClass("v-switcher_header_a_select");
+    
+    $(".v-switcher_header_anchor").removeClass("v-switcher_header_anchor_1");
+    $(".v-switcher_header_anchor").removeClass("v-switcher_header_anchor_2");
+    $(".v-switcher_header_anchor").removeClass("v-switcher_header_anchor_3");
+    $(".v-switcher_header_anchor").removeClass("v-switcher_header_anchor_4");
+    $(".v-switcher_header_anchor").removeClass("v-switcher_header_anchor_5");
+    $(".v-switcher_header_anchor").removeClass("v-switcher_header_anchor_6");
+
+    if (index == 0){
+        $(".v-switcher_header_anchor").addClass("v-switcher_header_anchor_1");
+    }else if(index == 1){
+        $(".v-switcher_header_anchor").addClass("v-switcher_header_anchor_2");
+    }else if(index == 2){
+        $(".v-switcher_header_anchor").addClass("v-switcher_header_anchor_3");
+    }else if(index == 3){
+        $(".v-switcher_header_anchor").addClass("v-switcher_header_anchor_4");
+    }else if(index == 4){
+        $(".v-switcher_header_anchor").addClass("v-switcher_header_anchor_5");
+    }else if(index == 5){
+        $(".v-switcher_header_anchor").addClass("v-switcher_header_anchor_6");
+    }
+
+})
